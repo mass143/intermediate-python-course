7d38e9d6495370988ad9d596e2e1f44a14f87240
mass143 <61025735+mass143@users.noreply.github.com>
2020-09-22 13:01:39 +0530

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -4,7 +4,7 @@ def main():
   dice_sum = 0
   for i in range(0, dice_rolls):
     roll = random.randint(1, 6)
-    dice_sum = (dice_sum + roll)/3
+    dice_sum = dice._sum +  roll 
     print(f'You rolled a {roll}')
   print(f'You have rolled a total of {dice_sum}')
 if __name__== "__main__":
